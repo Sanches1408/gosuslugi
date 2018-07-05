398bab29ce29bba70b305e6d8451bc8526469529
Sanches1408 <Sanches1408@gmail.com>
2018-07-05 17:39:26 +0500

    correct value object in forest.js and commenting dictionary Minzdrav

--- notice.js
+++ notice.js
@@ -2,11 +2,13 @@
 
   $(document).ready(function(){
 
-    var t = '<div class="alert alert-warning"><strong class="alert-title">Примечание</strong>\
-      Все группы прикрепляемых файлов, не помеченные символом *, не обязательны для прикрепления файлов,\
-      так как данные сведения запрашиваются органом государственной власти по форме межведомственного взаимодействия</div>';
+    var query = ''; // Селектор элемента, например: '#row_element', 'body', '.placeholder'
+    var note = '';
 
-    $('[data-grpid="gDocs"]').prepend($(t));
+    note = '<div class="group-promt group-promt__qPetit alert alert-warning" data-grpid="qPetit">\
+      <strong class="alert-title">Обратите внимание</strong><span><p class="alert">'+note+'</p></span></div>';
+
+    $('[data-grpid="gDocs"]').prepend($(note));
   });
 
 </script>
