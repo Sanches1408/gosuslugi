bda00d61317ea030e08be0afa2ca30d420b8b2b4
Sanches1408 <Sanches1408@gmail.com>
2018-07-06 16:17:34 +0500

    Functioanal dictionary is correct

--- notice.js
+++ notice.js
@@ -2,7 +2,14 @@
 
   $(document).ready(function(){
 
-    var query = ''; // Селектор элемента, например: '#row_element', 'body', '.placeholder'
+    /*
+      Селектор элемента, например: '#row_element', 'body', '.placeholder',
+      в начало которого вставляем уведомление
+    */
+    var query = '';
+    /*
+      Вводим свое сообщение, с ссылками и другими тегами, при необходимости
+    */
     var note = '';
 
     note = '<div class="group-promt group-promt__qPetit alert alert-warning" data-grpid="qPetit">\
--- Минздрав/dictionary/oneLvl.js
+++ Минздрав/dictionary/oneLvl.js
@@ -1,12 +1,6 @@
 <script>
 
 var timer = setInterval(function(){
-  console.log('timer');
-  debugger;
-  console.log(window.dictionary.tr);
-  console.log($(window.dictionary.tr));
-  console.log($(window.dictionary.tr)[2]);
-  console.log($('p', $(window.dictionary.tr)[2]).length);
   if ($('p', $(window.dictionary.tr)[2]).length == 151) {
     clearInterval(timer);
     start();
@@ -15,39 +9,18 @@ var timer = setInterval(function(){
 
 function start() {
   var w = window.dictionary;
-  console.log('start');
   $(w.tr+' li:contains("Выберите")').hide();
   $(w.tr+':not(:first-child)').hide();
 
   $(w.tr+':not(:last-of-type) li:not(:first-child)').each(function(){
     $(this).click(function(){
-      console.log('click');
-      //window.dictionary.hide(true);
-      //var lvl = window.dictionary.findOneLvl($(this).text());
-      w.show(w.find($(this).text()));
-      /*console.log(lvl);
-      if (lvl[1].length) {
-        $(lvl[0]).show();
-        $.each(lvl[1], function(i, v){ $(v).show(); });
-      }*/
+      w.find($(this).text());
     });
   });
-/*
-  $(tr+':nth-child(2) li:not(:first-child)').each(function(){
-    $(this).click(function(){
-      window.dictionary.hide();
-      var lvl = window.dictionary.findTwoLvl($(this).text());
-      console.log(lvl);
-      if (lvl[1].length) {
-        $(lvl[0]).show();
-        $.each(lvl[1], function(i, v){ $(v).show(); });
-      }
-    });
-  });
-*/
+
   var but = '<p class="attr-value checkbox" style="margin: 2vh 0;">\
     <input id="allChoice" type="checkbox" class="attr-value-el--filled" onclick="window.dictionary.checkAll(this);">\
-    <label for="allChoice"><span style="font-weight: bold; font-size: 14px;">Выбрать все</span></label></p>';
+    <label for="allChoice"><span id="checkToggle" style="font-weight: bold; font-size: 14px;" tog="Снять все">Выбрать все</span></label></p>';
   $(w.tr+':last-of-type .checkbox:first-of-type').before($(but));
 }
 
